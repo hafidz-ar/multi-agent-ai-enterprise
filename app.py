843a8994b194216c3d1d2f077c5e6d0b75d8237c
Hafidz <hafidzar93@gmail.com>
2026-07-21 21:57:10 +0700

    Revert app.py for HF Gradio trick

--- app.py
+++
@@ -1,8 +0,0 @@
-import os
-import uvicorn
-from api.main import app
-
-# Ini adalah trik agar FastAPI kita bisa berjalan di atas infrastruktur Gradio Hugging Face yang GRATIS
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
-    uvicorn.run(app, host="0.0.0.0", port=port)
